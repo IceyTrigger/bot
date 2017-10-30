e45596940139eefd01227424c09b008082c3f711
batshal <regmibatshal@gmail.com>
2017-10-30 17:02:05 -0400

    Update bot.py

--- bot.py
+++ bot.py
@@ -346,7 +346,7 @@ async def shutdown(self, ctx):
             await self.bot.say("Dictionoid shutting down...")
             await self.bot.logout()
             exit()
-        else: answer.lower() in ("n", "no"):
+        else: answer.lower() in ("n", "no")
             await self.bot.say("Bot not stopped!")
         else:
             await self.bot.say("Invalid answer! Bot not stopped!")
