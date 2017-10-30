4fc4464bdaa7c561853f3e2d9c2c806af9e78063
batshal <regmibatshal@gmail.com>
2017-10-29 20:14:54 -0400

    Update bot.py

--- bot.py
+++ bot.py
@@ -50,6 +50,23 @@ async def latency(ctx):
         await message.edit(delete_after = 15)
         await ctx.message.delete()
         
+@commands.command(pass_context = True)
+async def shutdown(ctx):
+    if ctx.message.author.id == bot_owner:293159670040887297
+        await bot.say("[y/n] Are you sure you want to stop the bot?\nIt will have to be started again manually.")
+        answer = await bot.wait_for_message(timeout = 30, author=ctx.message.author)
+        answer = str(answer)
+        if answer.lower() in ("y", "yes"):
+            await bot.say("Dictionoid shutting down...")
+            await bot.logout()
+            exit()
+        elif answer.lower() in ("n", "no"):
+            await bot.say("Bot not stopped!")
+        else:
+            await bot.say("Invalid answer! Bot not stopped!")
+    else:
+            await bot.say("You don't have permission to do that!")
+        
 @bot.command()
 @commands.has_permissions(kick_members = True)
 async def kick(ctx, user: discord.Member):
